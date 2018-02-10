944055dd1cebad3838fb554650276cdb92ccf3ef
Alexander Kostarev <UniverAlex@ya.ru>
2018-02-10 21:43:13 +0300

    If applied this commit will create PerfectlyBalancedTree test

---
+++ src/test/java/ru/univeralex/lab4trees/PerfectlyBalancedTreeTest.java
@@ -0,0 +1,10 @@
+package ru.univeralex.lab4trees;
+
+import org.junit.Test;
+
+public class PerfectlyBalancedTreeTest {
+    @Test
+    public void constructorTest() {
+        PerfectlyBalancedTree tree = new PerfectlyBalancedTree(10);
+    }
+}
