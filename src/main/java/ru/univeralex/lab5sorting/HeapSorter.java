e3fcba56bfb66f62d1a3b7b5b28be32f51bd411e
Alexander Kostarev <UniverAlex@ya.ru>
2018-02-17 18:48:27 +0300

    If applied this commit will add work for on getSorted() still debug needed.

--- src/main/java/ru/univeralex/lab5sorting/HeapSorter.java
+++ src/main/java/ru/univeralex/lab5sorting/HeapSorter.java
@@ -8,24 +8,21 @@ import java.util.Arrays;
 public class HeapSorter implements Sorter {
     @Override
     public int[] getSorted(int[] array) {
-        int[] startOnOneArray = new int[array.length + 1];
-        for (int i = 1; i < startOnOneArray.length; i++) startOnOneArray[i] = array[i - 1];
-
-        int N = startOnOneArray.length - 1;
-        for (int k = N / 2; k >= 1; k--) sink(startOnOneArray, k, N);
-        while (N > 1) {
-            ArrayUtils.swap(startOnOneArray, 1, N--);
-            sink(startOnOneArray, 1, N);
+        int[] sortingArray = Arrays.copyOf(array, array.length);
+        int N = sortingArray.length;
+        for (int k = N / 2; k > 0; k--) sink(sortingArray, k, sortingArray.length - 1);
+        while (N > 0) {
+            ArrayUtils.swap(sortingArray, 0, --N);
+            sink(sortingArray, 0, N);
         }
-        int[] resultingArray = new int[array.length];
-        Arrays.setAll(resultingArray, i -> startOnOneArray[i + 1]);
-        return resultingArray;
+        return sortingArray;
     }
 
     private void sink(int[] array, int k, int N) {
-        while (2 * k <= N) {
-            int j = 2 * k;
-            if (j < N && array[j] < array[j + 1]) j++;
+
+        while (2 * k + 1 < N) {
+            int j = 2 * k + 1;
+            if (j + 1 < N && array[j] < array[j + 1]) j++;
             if (array[k] > array[j]) break;
             ArrayUtils.swap(array, k, j);
             k = j;
